24dfb8e734312a9abbb885dfb51aa412af1bb5f3
joseLucas-code <skyleer05@gmail.com>
2025-03-03 13:33:27 -0300

    new: MesBtn

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
-import DateRange from './DateRange';
+import React from "react";
+import DateRange from "./DateRange";
+import Meses from "./Meses";
 
 const Header = () => {
   return (
     <header>
       <h2>Header</h2>
       <DateRange />
+      <Meses />
     </header>
   );
 };
---
+++ src/components/MesBtn.tsx
@@ -0,0 +1,36 @@
+import { useDataContext } from "../context/DataContext";
+
+function nomeMes(n: number) {
+  const date = new Date();
+  date.setMonth(date.getMonth() + n);
+  return date.toLocaleString("pt-BR", { month: "long" });
+}
+
+function formatDate(date: Date) {
+  const day = String(date.getDate()).padStart(2, "0");
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const year = String(date.getFullYear());
+  return `${year}-${month}-${day}`;
+}
+
+const MesBtn = ({ n }: { n: number }) => {
+  const { setInicio, setFinal } = useDataContext();
+  function setMes(n: number) {
+    const date = new Date();
+    date.setMonth(date.getMonth() + n);
+    const firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
+    const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+    setInicio(formatDate(firstDay));
+    setFinal(formatDate(lastDay));
+  }
+  return (
+    <button
+      className="w-full rounded-md bg-slate-200 px-6 py-2 text-black capitalize"
+      onClick={() => setMes(n)}
+    >
+      {nomeMes(n)}
+    </button>
+  );
+};
+
+export default MesBtn;
